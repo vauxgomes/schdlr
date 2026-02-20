51272027c702455765e528d44eefe1bd0c2c900c
Vaux Gomes <vauxsandino@gmail.com>
2026-02-20 13:39:41 -0300

    feat: setup phase 1 modules (users, accounts, subscriptions)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AccountsModule, SubscriptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
